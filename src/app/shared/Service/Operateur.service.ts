8fe446819cab78c87609049f2a3f67f1f67f05fc
oussemamansouri <oussemamansouri619@gmail.com>
2024-11-23 01:39:12 +0100

    Update Operateur.service.ts

--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://localhost:8082/SpringMVC/operateur';
+  readonly API_URL = 'http://localhost:8082/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
