cdf6c20da2379d657b4a62bf7aa5467271e02e3c
oussemamansouri <oussemamansouri619@gmail.com>
2024-11-23 01:39:10 +0100

    Update Facture.service.ts

--- src/app/shared/Service/Facture.service.ts
+++ src/app/shared/Service/Facture.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FactureService {
-  readonly API_URL = 'http://localhost:8082/SpringMVC/facture';
+  readonly API_URL = 'http://localhost:8082/facture';
 
   constructor(private httpClient: HttpClient) { }
 
